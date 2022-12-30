fc0dc7a201598855f700e6735778cd7a205e5eef
Damian Bugaj <damianbugajit@gmail.com>
2022-12-30 11:24:06 +0100

    add event

---
+++ js/script.js
@@ -0,0 +1,30 @@
+    /* document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+     });  */
+  
+
+  
+
+  const titleClickHandler = function(event){
+    console.log('Link was clicked!', event);
+  
+    /* remove class 'active' from all article links  */
+  
+    /* add class 'active' to the clicked link */
+  
+    /* remove class 'active' from all articles */
+  
+    /* get 'href' attribute from the clicked link */
+  
+    /* find the correct article using the selector (value of 'href' attribute) */
+  
+    /* add class 'active' to the correct article */
+  }
+  
+  const links = document.querySelectorAll('.titles a');
+  
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+  
