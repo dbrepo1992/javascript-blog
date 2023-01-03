05eacf531976a459f4849663fa22d1a8be1cf02f
Damian Bugaj <damianbugajit@gmail.com>
2023-01-03 19:51:33 +0100

    done task with articles and links

--- js/script.js
+++ js/script.js
@@ -1,33 +1,53 @@
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-  
+
     /* [DONE] remove class 'active' from all article links  */
-  
+
     const activeLinks = document.querySelectorAll('.titles a.active');
-  
+
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-  
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-  
+
+    /* [DONE] add class 'active' to the clicked link */
+
     clickedElement.classList.add('active')
 
     console.log('clickedElement:', clickedElement);
-  
+
     /* [DONE] remove class 'active' from all articles */
-  
+
+    const activeArticles = document.querySelectorAll('section article');
+
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
+
     /* get 'href' attribute from the clicked link */
-  
+
+    const articleSelector = clickedElement.getAttribute('href')
+
+    console.log(articleSelector);
+
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+
+    const targetArticle = document.querySelector(articleSelector)
+
+    console.log(targetArticle)
+
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active')
+
+    console.log('clicked element', targetArticle)
+
   }
-  
+
   const links = document.querySelectorAll('.titles a');
-  
+
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
-  
